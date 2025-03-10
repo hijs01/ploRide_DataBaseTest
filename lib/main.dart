13aec585bbfdcda3148793c273d088e1918efb7e
hijs01 <hijs0146@gmail.com>
2025-03-10 00:55:15 -0400

    lets try again

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        //이거해야함 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
