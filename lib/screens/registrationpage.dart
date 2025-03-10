a5e5e7a931327c0448cf0e0ae16c5a0dd0d3978b
Alex727727 <jks6598@psu.edu>
2025-03-10 02:28:29 -0400

    썰
    로그인 페이지 scaffold 싹다 긁어서 그 안에 항목들 이름정도 변경해서 그대로 거의 똑같게 생긴 페이지임.

---
+++ lib/screens/registrationpage.dart
@@ -0,0 +1,143 @@
+import 'package:cabrider/brand_colors.dart';
+import 'package:flutter/material.dart';
+
+class RegistrationPage extends StatelessWidget {
+  const RegistrationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 70),
+                Image(
+                  alignment: Alignment.center,
+                  height: 100.0,
+                  width: 100.0,
+                  image: AssetImage('images/logo.png'),
+                ),
+
+                SizedBox(height: 40),
+
+                Text(
+                  'Create a Rider\'s Account',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, fontFamily: 'brand-bold'),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    children: <Widget>[
+
+                      // Fullname
+                      TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          labelText: 'Full name',
+                          labelStyle: TextStyle(fontSize: 14),
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 14),
+                      ),
+
+                      SizedBox(height: 10,),
+
+                      // Email Address
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email Address',
+                          labelStyle: TextStyle(fontSize: 14),
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 14),
+                      ),
+
+                      SizedBox(height: 10),
+
+
+                      // Phone
+                      TextField(
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          labelText: 'Phone number',
+                          labelStyle: TextStyle(fontSize: 14),
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 14),
+                      ),
+
+                      SizedBox(height: 10),
+
+                      // Password
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(fontSize: 14),
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 14),
+                      ),
+
+                      SizedBox(height: 40),
+
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          backgroundColor: BrandColors.colorGreen,
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () {},
+                        child: Container(
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                              'REGISTER',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontFamily: 'Brand-Bold',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Already have a RIDER account? Log in',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
